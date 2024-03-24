d826eb4c092049db85059e2351ceadc964d7a1fa
Akanksha <akanksha@Akankshas-MacBook-Air.local>
2024-03-24 23:26:50 +0530

    CRUD operations

--- App.js
+++ App.js
@@ -3,15 +3,13 @@ import {
   Dimensions,
   FlatList,
   Modal,
-  SafeAreaView,
-  StatusBar,
+  Pressable,
   StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
   View,
 } from 'react-native';
-import firestore from '@react-native-firebase/firestore';
 import database from '@react-native-firebase/database';
 const App = () => {
   const inputRef = useRef();
@@ -22,12 +20,16 @@ const App = () => {
   const [isFocus, setIsFocus] = useState(false);
   const [isUpdate, setIsUpdate] = useState(false);
   const [selectedIndex, setSelectedIndex] = useState(null);
+
+  const [indexx, setIndexx] = useState(0);
+  const [item, setItem] = useState('');
+
   // Get user document with an ID of ABC
   useEffect(() => {
-    // getDatabase();
     getRealtimeData();
     console.log('called');
   }, []);
+  // getting data from  firebase firestore
   // const getDatabase = async () => {
   //   try {
   //     const data = await firestore()
@@ -40,7 +42,7 @@ const App = () => {
   //     console.log(error);
   //   }
   // };
-
+  const index = list?.length;
   const getRealtimeData = async () => {
     try {
       const realData = await database()
@@ -55,28 +57,54 @@ const App = () => {
   const handleChange = value => {
     setName(value);
   };
-  const index = list?.length;
+  console.log('list?.length', list?.length);
   const handleSubmit = async () => {
     try {
-      const sendDataToFirebase = await database().ref(`todo/${index}`).set({
-        value: name,
-      });
-      setName('');
+      if (name.length > 3) {
+        const sendDataToFirebase = await database().ref(`todo/${index}`).set({
+          value: name,
+        });
+        setName('');
+      }
     } catch (error) {
       console.log(error);
     }
   };
-  const handleEditPress = editName => {
+  const handleEditPress = (editName, index) => {
     setName(editName);
     inputRef?.current?.focus();
     setIsFocus(true);
     setIsUpdate(true);
+    setSelectedIndex(index);
   };
-  const handleUpdateSubmit = () => {
+  const handleUpdateSubmit = async () => {
     try {
-      setIsFocus(false);
-      setIsUpdate(false);
-      inputRef?.current?.blur();
+      if (name.length > 3) {
+        const response = await database()
+          .ref(`todo/${selectedIndex}`)
+          .update({value: name});
+        console.log('first', response);
+        console.log('Second', selectedIndex);
+        setIsFocus(false);
+        setIsUpdate(false);
+        inputRef?.current?.blur();
+        setName('');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleDeletePress = (item, index) => {
+    setEditModal(true);
+    setIndexx(index);
+    setItem(item);
+  };
+  const handleYesClick = async () => {
+    try {
+      const response = await database().ref(`todo/${indexx}`).remove();
+      const updatedList = list.filter((item, index) => index !== indexx);
+      setList(updatedList);
+      setEditModal(false);
     } catch (error) {
       console.log(error);
     }
@@ -94,6 +122,11 @@ const App = () => {
           onChangeText={handleChange}
           value={name}
         />
+        {name?.length < 3 && name?.length > 0 && (
+          <Text style={style.error}>
+            Input length must be at least 3 characters
+          </Text>
+        )}
         {isUpdate ? (
           <TouchableOpacity
             style={style.tch}
@@ -111,9 +144,10 @@ const App = () => {
         <FlatList
           data={list}
           renderItem={item => {
-            console.log('first', item?.index);
             return (
-              <View style={style.card}>
+              <TouchableOpacity
+                style={style.card}
+                onLongPress={() => setEditModal(true)}>
                 {item?.item?.value?.length < 25 ? (
                   <Text style={style.text}>Name : {item?.item?.value}</Text>
                 ) : (
@@ -124,29 +158,101 @@ const App = () => {
 
                 <View style={style.tchEdit}>
                   <TouchableOpacity
-                    onPress={() => handleEditPress(item?.item?.value)}>
+                    onPress={() =>
+                      handleEditPress(item?.item?.value, item?.index)
+                    }>
                     <Text style={style.edit}>Edit</Text>
                   </TouchableOpacity>
                   <TouchableOpacity
                     style={style.delete}
-                    onPress={() => setEditModal(true)}>
+                    onPress={() =>
+                      handleDeletePress(item?.item?.value, item?.index)
+                    }>
                     <Text style={style.deleteText}>Delete</Text>
                   </TouchableOpacity>
                 </View>
-              </View>
+              </TouchableOpacity>
             );
           }}
           showsVerticalScrollIndicator={false}
         />
       </View>
-      <Modal visible={editModal} />
+      <Modal visible={editModal} transparent={true}>
+        <View style={style.modalContainer}>
+          <View style={style.modalContainer2}>
+            <View style={style.main}>
+              <Text style={style.deleteTitle}>Delete Details</Text>
+              <Text style={style.subheading}>
+                Are you sure you want to delete {item}?
+              </Text>
+              <View style={style.bottomBtn}>
+                <Pressable style={style.yesBtn} onPress={handleYesClick}>
+                  <Text style={style.yesText}>Yes</Text>
+                </Pressable>
+                <Pressable
+                  style={style.noBtn}
+                  onPress={() => setEditModal(false)}>
+                  <Text style={style.yesText}>No</Text>
+                </Pressable>
+              </View>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 };
+const COLOR_WHITE_70_OPACITY = 'rgba(0,0,0,0.7)';
 const {height, width} = Dimensions.get('screen');
 const style = StyleSheet.create({
   tchEdit: {
     flexDirection: 'row',
+  },
+  yesText: {
+    color: 'white',
+    fontSize: 16,
+  },
+  yesBtn: {
+    backgroundColor: 'green',
+    paddingVertical: 5,
+    paddingHorizontal: 15,
+  },
+  noBtn: {
+    backgroundColor: 'red',
+    paddingVertical: 5,
+    paddingHorizontal: 17,
+    marginStart: 10,
+  },
+  bottomBtn: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    marginHorizontal: 8,
+    marginTop: 15,
+  },
+  subheading: {
+    marginTop: 10,
+  },
+  deleteTitle: {
+    color: 'black',
+    fontSize: 18,
+  },
+  modalContainer: {
+    flex: 1,
+    backgroundColor: COLOR_WHITE_70_OPACITY,
+  },
+  modalContainer2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  main: {
+    height: 'auto',
+    padding: 27,
+    width: width - 50,
+    backgroundColor: 'white',
+  },
+  error: {
+    color: 'red',
   },
   edit: {
     color: 'blue',
